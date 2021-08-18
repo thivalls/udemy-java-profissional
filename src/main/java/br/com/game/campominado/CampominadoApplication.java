45e13d8d7fef2f80a1abfa258fdb173516eebb13
valls <olavalls@gmail.com>
2021-08-17 23:43:13 -0300

    Criando projeto inicial

---
+++ src/main/java/br/com/game/campominado/CampominadoApplication.java
@@ -0,0 +1,13 @@
+package br.com.game.campominado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampominadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampominadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/game/campominado/CampominadoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.game.campominado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampominadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
