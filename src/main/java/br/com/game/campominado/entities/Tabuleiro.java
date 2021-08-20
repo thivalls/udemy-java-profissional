5714d54c548fed88d2ec579502718a758c6f2f57
valls <olavalls@gmail.com>
2021-08-19 23:27:51 -0300

    Criando classe tabuleiro

--- src/main/java/br/com/game/campominado/entities/Tabuleiro.java
+++ src/main/java/br/com/game/campominado/entities/Tabuleiro.java
@@ -1,4 +1,65 @@
 package br.com.game.campominado.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
 public class Tabuleiro {
+    private int linhas;
+    private int colunas;
+    private int minas;
+
+    private final List<Campo> campos = new ArrayList<>();
+
+    public Tabuleiro(int linhas, int colunas, int minas) {
+        this.linhas = linhas;
+        this.colunas = colunas;
+        this.minas = minas;
+
+        gerarCampos();
+        associarVizinhos();
+        gerarMinas();
+    }
+
+
+    private void gerarCampos() {
+        for (int linha = 0; linha < linhas; linha++) {
+            for (int coluna = 0; coluna < colunas; coluna++) {
+                campos.add(new Campo(linha, coluna));
+            }
+        }
+    }
+
+    private void associarVizinhos() {
+        for (Campo campo1 : campos) {
+            for (Campo campo2 : campos) {
+                campo1.adicionarVizinho(campo2);
+            }
+        }
+    }
+
+    private void gerarMinas() {
+        long minasArmadas = 0;
+        Predicate<Campo> minado = Campo::isMinado;
+        do {
+            minasArmadas = campos.stream().filter(minado).count();
+            int aleatorio = (int) (Math.random() + campos.size());
+            if (!campos.get(aleatorio).isMinado()) {
+                campos.get(aleatorio).minar();
+            }
+        } while (minasArmadas < minas);
+    }
+
+    boolean objetivoAlcancado() {
+        return campos.stream().allMatch(c -> c.objetivoAlcancado());
+    }
+
+    public void reiniciar() {
+        campos.stream().forEach(c -> c.reiniciar());
+        gerarMinas();
+    }
+
+    public String toString() {
+        return "";
+    }
 }
